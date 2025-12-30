a12ee93d2cd53091a0d9b2458888d5586550deab
Jonathan Richard <jwrichard26@gmail.com>
2025-12-30 17:27:56 -0500

    Update local.rs

--- crates/burn-central-workspace/src/execution/local.rs
+++ crates/burn-central-workspace/src/execution/local.rs
@@ -715,7 +715,7 @@ impl<'a> LocalExecutor<'a> {
 }
 
 #[derive(Serialize)]
-pub struct BurnCentralArgs {
+struct BurnCentralArgs {
     pub namespace: String,
     pub project: String,
     pub api_key: String,
@@ -723,21 +723,10 @@ pub struct BurnCentralArgs {
 }
 
 #[derive(Serialize)]
-/// Arguments provided via CLI by the Burn Central CLI
-pub struct RuntimeArgs {
-    /// The device ids to use for the routine execution. If not provided, the default device will be
-    /// used.
+struct RuntimeArgs {
     pub devices: Option<Vec<DeviceIdPair>>,
-    /// The kind of routine to execute. It can be `training` or `inference`.
     pub kind: String,
-    /// The name of the routine to execute. We pass the routine name here as the name might not be
-    /// the name of the function if the user decide to rename it using the `name` attribute in the
-    /// register macro.
     pub routine: String,
-    /// JSON string representing the arguments to pass to the routine. The arguments pass here are
-    /// self define by the user. Value found in this field will be merge with the Config the user
-    /// is requesting using [Args] extractor in his training function.
     pub args: String,
-    /// Burn Central configuration arguments.
     pub burn_central: BurnCentralArgs,
 }
